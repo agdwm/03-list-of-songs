2c90a4192aa7e8e7b6a0d8028da462b1b2d5711a
agdwm <almudenaguerraswm@gmail.com>
2017-08-09 20:36:11 +0200

    Created new method to render songs

--- src/js/services/SongsListManager.js
+++ src/js/services/SongsListManager.js
@@ -1,4 +1,5 @@
-const $ = require("jquery");
+//En este servicio no es necesario importar jQuery porque no se está utilizando
+//const $ = require("jquery");
 
 export default class SongsListManager {
 
@@ -6,15 +7,16 @@ export default class SongsListManager {
         this.songsService = songsService;
         this.songsListUIManager = songsListUIManager;
     }
-
-    //El método init() es sólo para INICIALIZAR el componente
+    //Cada método debería hacer solo una única cosa
+    //init() sólo inicializa el componente
     init() {
         this.loadSongs();
     }
-
+    //loadSongs() solo carga las canciones
     loadSongs() {
         //SongService.list(songs => {},  error => {});
-        /*Llamamos al método "list()" del objeto "songService" y le pasamos 2 funciones que gestionará el objeto en caso de éxito o error de la llamada Ajax.*/
+        /*Llamamos al método "list()" del objeto "songService" y le pasamos 2 funciones que gestionará 
+        el objeto en caso de éxito o error de la llamada Ajax.*/
         this.songsService.list(
             songs => {
                 //Comprobamos si hay canciones
@@ -23,8 +25,7 @@ export default class SongsListManager {
                     this.songsListUIManager.setEmpty();
                 } else {
                     //Componemos el html con todas las canciones
-
-
+                    this.renderSongs(songs);
                     //Quitamos el mensaje de cargando y mostramos la lista de canciones
                     this.songsListUIManager.setIdeal();
                 }
@@ -32,7 +33,6 @@ export default class SongsListManager {
             error => {
                 //Mostramos el estado de error
                 this.songsListUIManager.setError();
-
                 //Hacemos log del error en la consola
                 console.log("ERROR al cargar las canciones. :(", error);
             });
@@ -40,17 +40,22 @@ export default class SongsListManager {
         this.songsService.getDetail(5);
     }
 
+    //renderSongs() pinta las canciones en el html
     renderSongs(songs) {
         let html = "";
         for (let song of songs) {
-            html +=
-                `<article class="song">
+            html += this.renderSong(song)
+        }
+        //Metemos el HTML en el div que contiene las canciones
+        this.songsListUIManager.setIdealHtml(html);
+    }
+    
+    //renderiza una única canción
+    renderSong(song) {
+        return `<article class="song">
                     <img src="${song.cover_url}" alt="${song.artist} - ${song.title}" class="cover">
                     <div class="artist">${song.artist}</div>
                     <div class="title">${song.title}</div>
                 </article>`;
-        }
-        //Metemos el HTML en el div que contiene las canciones
-        $(".songs-list .ui-status.ideal").html(html);
     }
 }
